520cec92bfbc9a4566a23d6896aba6ac0072d94f
Marcos Felipe Rocha <marcos.fellps@gmail.com>
2021-10-08 17:19:03 -0300

    Create CheckPoint-01.swift

---
+++ Checkpoints/01/CheckPoint-01.swift
@@ -0,0 +1,11 @@
+/*
+1. Cria uma constante mantendo qualquer temperatura em Celsius.
+2. Converte em Fahrenheit multiplicando por 9, dividindo por 5 e, em seguida, adicionando 32.
+3. Imprime o resultado para o usuário, mostrando os valores Celsius e Fahrenheit.
+*/    
+
+let Celsius:Double = 100
+var Fahrenheit:Double = Celsius * 9.0 / 5.0 + 32.0
+print("\(Fahrenheit) ºF")
+
+// 212.0 Fº
